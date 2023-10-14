fe519db6ab2184cb7ac1c794e6d1289b44a94f7e
Johnny Tao <ahalamora@hotmail.com>
2023-10-14 18:54:07 +0800

    modified:   package/doc_packing.py 	new file:   package/doc_packing_v2.py 	new file:   package/docx_to_pdf.py 	new file:   program_v2/doc_packing_v2.py

---
+++ package/docx_to_pdf.py
@@ -0,0 +1,30 @@
+import os
+import win32com.client
+from tqdm import tqdm
+
+
+wdFormatPDF = 17
+
+BASE_PATH = R'Documents\2023\10\文档自动归类\input'
+
+INPUT_FOLDER = '身份证_docx'
+OUTPUT_FOLDER = '身份证_pdf'
+
+INPUT_PATH = os.path.join(BASE_PATH, INPUT_FOLDER)
+OUTPUT_PATH = os.path.join(BASE_PATH, OUTPUT_FOLDER)
+
+if OUTPUT_FOLDER not in os.listdir(BASE_PATH):
+    os.mkdir(OUTPUT_PATH)
+
+word = win32com.client.Dispatch("Word.Application")
+
+for file in tqdm(os.listdir(INPUT_PATH)):
+
+    inputFile = os.path.abspath(os.path.join(INPUT_PATH, file))
+    outputFile = os.path.abspath(os.path.join(OUTPUT_PATH, file.strip('.docx')+'.pdf'))
+    
+    docx = word.Documents.Open(inputFile)
+    docx.SaveAs(outputFile, FileFormat=wdFormatPDF)
+
+docx.Close()
+word.Quit()
